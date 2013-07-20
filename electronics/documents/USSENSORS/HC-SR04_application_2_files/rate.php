71df451b7026f9ef67bf1a9560fb7de67965c9ab
maska <maska@mail.ru>
2013-07-20 19:36:54 +0300

    electronics:circuits platform location p1 PCB initial commit; check CD4067 to transfer own VCC as a signal.

---
+++ electronics/documents/USSENSORS/HC-SR04_application_2_files/rate.php
@@ -0,0 +1,30 @@
+PDRTJS_5506978_post_5920.avg_rating = 5;
+PDRTJS_5506978_post_5920.votes = 10;		
+PDRTJS_settings_5506978_post_5920= {
+"type" : "stars",
+"size" : "sml",
+"star_color" : "yellow",
+"custom_star" : "",
+"font_size" : "",
+"font_line_height" : "16px",
+"font_color" : "",
+"font_align" : "left",
+"font_position" : "right",
+"font_family" : "",
+"font_bold" : "normal",
+"font_italic" : "normal",
+"text_vote" : "Vote",
+"text_votes" : "Votes",
+"text_rate_this" : "Rate This",
+"text_1_star" : "Very Poor",
+"text_2_star" : "Poor",
+"text_3_star" : "Average",
+"text_4_star" : "Good",
+"text_5_star" : "Excellent",
+"text_thank_you" : "Thank You",
+"text_rate_up" : "Rate Up",
+"text_rate_down" : "Rate Down"
+};
+PDRTJS_5506978_post_5920.init();		
+PDRTJS_5506978_post_5920.token='60ae7d3df798d1df88bdf768a927582c';
+/*5506978,_post_5920,wp-post-5920,1383459187,5-10*/
