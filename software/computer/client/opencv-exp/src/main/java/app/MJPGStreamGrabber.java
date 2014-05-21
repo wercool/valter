b1d72d375605aba0440aec4f9b1ddaafc6b1905f
maska <maska@sashimi>
2014-05-21 19:26:23 +0300

    gb-08-m2: harra_cascade experiments in java

--- software/computer/client/opencv-exp/src/main/java/app/MJPGStreamGrabber.java
+++ software/computer/client/opencv-exp/src/main/java/app/MJPGStreamGrabber.java
@@ -7,8 +7,14 @@ import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.highgui.VideoCapture;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
 public class MJPGStreamGrabber
 {
@@ -40,11 +46,39 @@ public class MJPGStreamGrabber
     public void stopCapture()
     {
         isCapturing = false;
-        capture.release();
+        if (capture != null)
+        {
+            if (capture.isOpened())
+            {
+                capture.release();
+            }
+        }
     }
 
     private void outputFrame()
     {
+        CascadeClassifier objectDetector = new CascadeClassifier("/home/maska/Desktop/classifier/cascade.xml");
+        Mat mRgba = new Mat();
+        Mat mGrey = new Mat();
+        MatOfRect detectedObjects = new MatOfRect();
+        MJPGMatFrame.copyTo(mRgba);
+        MJPGMatFrame.copyTo(mGrey);
+        Imgproc.cvtColor(mRgba, mGrey, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.equalizeHist(mGrey, mGrey);
+        objectDetector.detectMultiScale(mGrey, detectedObjects);
+        //Size minSize = new Size(40, 20);
+        //Size maxSize = new Size(50, 100);
+        //objectDetector.detectMultiScale(mGrey, detectedObjects, 1.1, 2, Objdetect.CASCADE_DO_CANNY_PRUNING | Objdetect.CASCADE_DO_ROUGH_SEARCH, minSize, maxSize);
+        System.out.println(String.format("Detected %s objects", detectedObjects.toArray().length));
+
+        Rect[] detectedObjectsArray = detectedObjects.toArray();
+        for (int i = 0; i < detectedObjectsArray.length; i++)
+        {
+            Core.rectangle(mRgba, detectedObjectsArray[i].tl(), detectedObjectsArray[i].br(), new Scalar(0, 255, 0), 1);
+        }
+
+        MJPGframe = matToBufferedImage(mRgba);
+
         Image image = SwingFXUtils.toFXImage(MJPGframe, null);
         streamOutputView.setImage(image);
     }
@@ -97,7 +131,7 @@ public class MJPGStreamGrabber
                 capture.read(MJPGMatFrame);
                 if (!MJPGMatFrame.empty())
                 {
-                    MJPGframe = matToBufferedImage(MJPGMatFrame);
+                    //MJPGframe = matToBufferedImage(MJPGMatFrame);
                     outputFrame();
                     try
                     {
