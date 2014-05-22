60b45844829cb77894b51d7e2c2f66de632e78bc
maska <maska@sashimi>
2014-05-22 18:54:27 +0300

    gb-08-m2: HAAR detectection added, template matching added

--- software/computer/client/opencv-exp/src/main/java/app/MJPGStreamGrabber.java
+++ software/computer/client/opencv-exp/src/main/java/app/MJPGStreamGrabber.java
@@ -8,13 +8,21 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import org.opencv.core.Core;
+import org.opencv.core.Core.MinMaxLocResult;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfDouble;
+import org.opencv.core.MatOfInt;
 import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.highgui.Highgui;
 import org.opencv.highgui.VideoCapture;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.objdetect.Objdetect;
 
 public class MJPGStreamGrabber
 {
@@ -26,6 +34,10 @@ public class MJPGStreamGrabber
     private BufferedImage MJPGframe;
     private VideoCapture capture;
     private Thread captureFrameThread;
+
+    //HAAR settings
+    private int neighboursNum = 1;
+    Mat template;
 
     public MJPGStreamGrabber(String streamURL, ImageView streamOutputView)
     {
@@ -41,6 +53,9 @@ public class MJPGStreamGrabber
         isCapturing = true;
         captureFrameThread = new Thread(new RunnableCaptureFrame(), "frontCameraCaptureFrameThread");
         captureFrameThread.start();
+
+        //template = Highgui.imread("/home/maska/Desktop/door-flap.jpg");
+        template = Highgui.imread("/home/maska/Desktop/object.jpg");
     }
 
     public void stopCapture()
@@ -57,7 +72,8 @@ public class MJPGStreamGrabber
 
     private void outputFrame()
     {
-        CascadeClassifier objectDetector = new CascadeClassifier("/home/maska/Desktop/classifier/cascade.xml");
+        //CascadeClassifier objectDetector = new CascadeClassifier("/home/maska/Desktop/classifier/cascade.xml");
+        CascadeClassifier objectDetector = new CascadeClassifier("/home/maska/Desktop/door/cascade.xml");
         Mat mRgba = new Mat();
         Mat mGrey = new Mat();
         MatOfRect detectedObjects = new MatOfRect();
@@ -65,18 +81,68 @@ public class MJPGStreamGrabber
         MJPGMatFrame.copyTo(mGrey);
         Imgproc.cvtColor(mRgba, mGrey, Imgproc.COLOR_BGR2GRAY);
         Imgproc.equalizeHist(mGrey, mGrey);
-        objectDetector.detectMultiScale(mGrey, detectedObjects);
-        //Size minSize = new Size(40, 20);
-        //Size maxSize = new Size(50, 100);
-        //objectDetector.detectMultiScale(mGrey, detectedObjects, 1.1, 2, Objdetect.CASCADE_DO_CANNY_PRUNING | Objdetect.CASCADE_DO_ROUGH_SEARCH, minSize, maxSize);
+        //        objectDetector.detectMultiScale(mGrey, detectedObjects);
+        //        Size minSize = new Size(50, 50);
+        //        Size maxSize = new Size(200, 200);
+        //        objectDetector.detectMultiScale(mGrey, detectedObjects, 1.1, 3, Objdetect.CASCADE_DO_CANNY_PRUNING | Objdetect.CASCADE_DO_ROUGH_SEARCH, minSize, maxSize);
+
+        MatOfInt rejectLevels = new MatOfInt();
+        MatOfDouble levelWeights = new MatOfDouble();
+        double scaleFactor = 1.1;
+        //int minNeighbors = 37;
+        int deprecatedFlag = 0;
+        Size minSize = new Size(100, 100);
+        Size maxSize = new Size(400, 400);
+        boolean outputRejectLevels = false;
+
+        //objectDetector.detectMultiScale(mGrey, detectedObjects, rejectLevels, levelWeights);
+        objectDetector.detectMultiScale(mGrey, detectedObjects, rejectLevels, levelWeights, scaleFactor, neighboursNum, Objdetect.CASCADE_DO_CANNY_PRUNING | Objdetect.CASCADE_DO_ROUGH_SEARCH, new Size(), new Size(), outputRejectLevels);
+
+        double[] weights = new double[0];
+
+        if (levelWeights.size().width > 0)
+        {
+            weights = levelWeights.toArray();
+        }
+
         System.out.println(String.format("Detected %s objects", detectedObjects.toArray().length));
 
         Rect[] detectedObjectsArray = detectedObjects.toArray();
         for (int i = 0; i < detectedObjectsArray.length; i++)
         {
+            if (levelWeights.size().width > 0)
+            {
+                System.out.println("weight [" + i + "]: " + weights[i]);
+            }
             Core.rectangle(mRgba, detectedObjectsArray[i].tl(), detectedObjectsArray[i].br(), new Scalar(0, 255, 0), 1);
+            Core.putText(mRgba, Integer.toString(i), detectedObjectsArray[i].tl(), Core.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 255, 0));
         }
 
+        //Core.rectangle(mRgba, detectedObjectsArray[detectedObjectsArray.length - 1].tl(), detectedObjectsArray[detectedObjectsArray.length - 1].br(), new Scalar(0, 255, 0), 1);
+        //Core.putText(mRgba, Integer.toString(detectedObjectsArray.length - 1), detectedObjectsArray[detectedObjectsArray.length - 1].tl(), Core.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 255, 0));
+
+        //        if (detectedObjectsArray.length > 1)
+        //        {
+        //            neighboursNum++;
+        //            System.out.println(String.format("neighboursNum = %d", neighboursNum));
+        //        }
+
+        //Template Matching
+        int result_cols = mRgba.cols() - template.cols() + 1;
+        int result_rows = mRgba.rows() - template.rows() + 1;
+        Mat result = new Mat(result_rows, result_cols, CvType.CV_32FC1);
+
+        // / Do the Matching and Normalize
+        Imgproc.matchTemplate(mRgba, template, result, Imgproc.TM_CCOEFF);
+        Core.normalize(result, result, 0, 1, Core.NORM_MINMAX, -1, new Mat());
+
+        // / Localizing the best match with minMaxLoc
+        MinMaxLocResult mmr = Core.minMaxLoc(result);
+        Point matchLoc = mmr.maxLoc;
+
+        Core.rectangle(mRgba, matchLoc, new Point(matchLoc.x + template.cols(), matchLoc.y + template.rows()), new Scalar(0, 0, 255));
+
+        //Frame Output
         MJPGframe = matToBufferedImage(mRgba);
 
         Image image = SwingFXUtils.toFXImage(MJPGframe, null);
