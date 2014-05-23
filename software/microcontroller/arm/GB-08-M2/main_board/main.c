7bd0c65757dff000240240074bcc3922258e4548
maska <maska@mail.ru>
2014-05-24 00:43:54 +0300

    gb-08-m2: radar servo control added

--- software/microcontroller/arm/GB-08-M2/main_board/main.c
+++ software/microcontroller/arm/GB-08-M2/main_board/main.c
@@ -31,8 +31,6 @@ unsigned int servoSignalWidth = 0;
 unsigned int radarServoRotation = 0;
 unsigned char radarServoSet = 0;
 
-//temp
-unsigned tempPeriod = 10;
 
 /*-----------------*/
 /* Clock Selection */
@@ -123,18 +121,35 @@ __ramfunc void FIQHandler(void)
 
 __ramfunc void SoftIQRHandlerTC0(void)
 {
-    AT91PS_TC TC_pt = AT91C_BASE_TC0;
-    unsigned int dummy;
-    //* Acknowledge interrupt status
-    dummy = TC_pt->TC_SR;
+    volatile AT91PS_TC pTC = AT91C_BASE_TC0; /* pointer to timer channel 0 register structure */
+    volatile unsigned int dummy; /* temporary */
+
+    /* read TC0 Status Register to clear interrupt */
+    dummy = pTC->TC_SR;
     //* Suppress warning variable "dummy" was set but never used
     dummy = dummy;
 
-    if (timerLeft == 0)
-        timerLeft = 1;
-
-    if (timerRight == 0)
-        timerRight = 1;
+    if (radarServoSet)
+    {
+        if (servoSignalPeriod < 1000)
+        {
+            servoSignalPeriod++;
+        }
+        else
+        {
+            if (servoSignalWidth < radarServoRotation / 20)
+            {
+                AT91F_PIO_SetOutput(AT91C_BASE_PIOA, AT91C_PIO_PA27);
+                servoSignalWidth++;
+            }
+            else
+            {
+                AT91F_PIO_ClearOutput(AT91C_BASE_PIOA, AT91C_PIO_PA27);
+                servoSignalWidth = 0;
+                servoSignalPeriod = 0;
+            }
+        }
+    }
 }
 
 __ramfunc void SoftIQRHandlerTC1(void)
@@ -146,40 +161,11 @@ __ramfunc void SoftIQRHandlerTC1(void)
     //* Suppress warning variable "dummy" was set but never used
     dummy = dummy;
 
-//    // Servo software signal generation
-//    if (radarServoSet)
-//    {
-//        if (servoSignalPeriod > tempPeriod)
-//        {
-//            if (servoSignalWidth < radarServoRotation)
-//            {
-//                AT91F_PIO_SetOutput(AT91C_BASE_PIOA, AT91C_PIO_PA27);
-//                servoSignalWidth++;
-//            }
-//            else
-//            {
-//                AT91F_PIO_ClearOutput(AT91C_BASE_PIOA, AT91C_PIO_PA27);
-//                servoSignalPeriod = 0;
-//                servoSignalWidth = 0;
-//            }
-//        }
-//        else
-//        {
-//            servoSignalPeriod++;
-//        }
-//    }
+    if (timerLeft == 0)
+        timerLeft = 1;
 
-    if (radarServoSet)
-    {
-        radarServoSet = 0;
-        AT91F_PIO_ClearOutput(AT91C_BASE_PIOA, AT91C_PIO_PA27);
-    }
-    else
-    {
-        radarServoSet = 1;
-        AT91F_PIO_SetOutput(AT91C_BASE_PIOA, AT91C_PIO_PA27);
-    }
-
+    if (timerRight == 0)
+        timerRight = 1;
 }
 
 /*
@@ -321,6 +307,284 @@ static void InitPIO(void)
     AT91C_BASE_PIOA->PIO_PPUDR = ~0;
 }
 
+static void Timer0Setup()
+{
+    /* enable the Timer0 peripheral clock */
+    volatile AT91PS_PMC pPMC = AT91C_BASE_PMC;
+    pPMC->PMC_PCER = pPMC->PMC_PCSR | (1 << AT91C_ID_TC0);
+    /* Set up the AIC registers for Timer 0 */
+    volatile AT91PS_AIC pAIC = AT91C_BASE_AIC;
+    /* Disable timer 0 interrupt */
+    /* in AIC Interrupt Disable Command Register */
+    pAIC->AIC_IDCR = (1 << AT91C_ID_TC0);
+    /* Set the TC0 IRQ handler address in */
+    /* AIC Source Vector Register[12] */
+    pAIC->AIC_SVR[AT91C_ID_TC0] = (unsigned int) SoftIQRHandlerTC0;
+    /* Set the interrupt source type and priority */
+    /* in AIC Source Mode Register[12] */
+    pAIC->AIC_SMR[AT91C_ID_TC0] = (AT91C_AIC_SRCTYPE_INT_HIGH_LEVEL | 0x4);
+    /* Clear the TC0 interrupt */
+    /* in AIC Interrupt Clear Command Register */
+    pAIC->AIC_ICCR = (1 << AT91C_ID_TC0);
+    /* Remove disable timer 0 interrupt */
+    /* in AIC Interrupt Disable Command Reg */
+    pAIC->AIC_IDCR = (0 << AT91C_ID_TC0);
+    /* Enable the TC0 interrupt */
+    /* in AIC Interrupt Enable Command Register */
+    pAIC->AIC_IECR = (1 << AT91C_ID_TC0);
+    /* Setup timer0 to generate a 1 msec periodic interrupt */
+
+
+    /* TC Block Control Register TC_BCR (read/write) */
+    /* */
+    /* |------------------------------------------------------------------|------| */
+    /* | SYNC | */
+    /* |------------------------------------------------------------------|------| */
+    /* 31 1 0 */
+    /* */
+    /* SYNC = 0 (no effect) <===== take default */
+    /* SYNC = 1 (generate software trigger for all 3 timer channels simultaneously) */
+    /* */
+    /* create a pointer to TC Global Register structure */
+    AT91PS_TCB pTCB = AT91C_BASE_TCB;
+    /* SYNC trigger not used */
+    pTCB->TCB_BCR = 0;
+
+    /* TC Block Mode Register TC_BMR (read/write) */
+    /* */
+    /* |-------------------------------------|-----------|-----------|-----------| */
+    /* | TC2XC2S TCXC1S TC0XC0S | */
+    /* |-------------------------------------|-----------|-----------|-----------| */
+    /* 31 5 4 3 2 1 0 */
+    /* */
+    /* TC0XC0S Select = 00 TCLK0 (PA4) */
+    /* = 01 none <===== we select this one */
+    /* = 10 TIOA1 (PA15) */
+    /* = 11 TIOA2 (PA26) */
+    /* */
+    /* TCXC1S Select = 00 TCLK1 (PA28) */
+    /* = 01 none <===== we select this one */
+    /* = 10 TIOA0 (PA15) */
+    /* = 11 TIOA2 (PA26) */
+    /* */
+    /* TC2XC2S Select = 00 TCLK2 (PA29) */
+    /* = 01 none <===== we select this one */
+    /* = 10 TIOA0 (PA00) */
+    /* = 11 TIOA1 (PA26) */
+    /* */
+    /* external clocks not used */
+    pTCB->TCB_BMR = 0x15;
+
+
+    /* TC Channel Control Register TC_CCR (read/write) */
+    /* */
+    /* |----------------------------------|--------------|------------|-----------| */
+    /* | SWTRG CLKDIS CLKENS | */
+    /* |----------------------------------|--------------|------------|-----------| */
+    /* 31 2 1 0 */
+    /* */
+    /* CLKEN = 0 no effect */
+    /* CLKEN = 1 enables the clock <===== we select this one */
+    /* */
+    /* CLKDIS = 0 no effect <===== take default */
+    /* CLKDIS = 1 disables the clock */
+    /* */
+    /* SWTRG = 0 no effect */
+    /* SWTRG = 1 software trigger aserted counter reset and clock starts <===== we select this one */
+    /* */
+    /* create a pointer to channel 0 Register structure */
+    AT91PS_TC pTC = AT91C_BASE_TC0;
+    /* enable the clock and start it */
+    pTC->TC_CCR = 0x5;
+
+    /* TC Channel Mode Register TC_CMR (read/write) */
+    /* */
+    /* |-----------------------------------|------------|---------------| */
+    /* | LDRB LDRA | */
+    /* |-----------------------------------|------------|---------------| */
+    /* 31 19 18 17 16 */
+    /* */
+    /* |----------|---------|--------------|------------|---------------| */
+    /* |WAVE = 0 CPCTRG ABETRG ETRGEDG | */
+    /* |----------|---------|--------------|------------|---------------| */
+    /* 15 14 13 11 10 9 8 */
+    /* */
+    /* |----------|---------|--------------|------------|---------------| */
+    /* | LDBDIS LDBSTOP BURST CLKI TCCLKS | */
+    /* |----------|---------|--------------|------------|---------------| */
+    /* 7 6 5 4 3 2 0 */
+    /* */
+    /* CLOCK SELECTION */
+    /* TCCLKS = 000 TIMER_CLOCK1 (MCK/2 = 24027420 hz) */
+    /* 001 TIMER_CLOCK2 (MCK/8 = 6006855 hz) */
+    /* 010 TIMER_CLOCK3 (MCK/32 = 1501713 hz) */
+    /* 011 TIMER_CLOCK4 (MCK/128 = 375428 hz) */
+    /* 100 TIMER_CLOCK5 (MCK/1024 = 46928 hz) <===== we select this one */
+    /* 101 XC0 */
+    /* 101 XC1 */
+    /* 101 XC2 */
+    /* */
+    /* CLOCK INVERT */
+    /* CLKI = 0 counter incremented on rising clock edge <===== we select this one */
+    /* CLKI = 1 counter incremented on falling clock edge */
+    /* */
+    /* BURST SIGNAL SELECTION */
+    /* BURST = 00 clock is not gated by any external system <===== take default */
+    /* 01 XC0 is anded with the clock */
+    /* 10 XC1 is anded with the clock */
+    /* 11 XC2 is anded with the clock */
+    /* */
+    /* COUNTER CLOCK STOPPED WITH RB LOADING */
+    /* LDBSTOP = 0 counter clock is not stopped when RB loading occurs <===== take default */
+    /* = 1 counter clock is stopped when RB loading occur */
+    /* */
+    /* COUNTER CLOCK DISABLE WITH RB LOADING */
+    /* LDBDIS = 0 counter clock is not disabled when RB loading occurs <===== take default */
+    /* = 1 counter clock is disabled when RB loading occurs */
+    /* */
+    /* EXTERNAL TRIGGER EDGE SELECTION */
+    /* ETRGEDG = 00 (none) <===== take default */
+    /* 01 (rising edge) */
+    /* 10 (falling edge) */
+    /* 11 (each edge) */
+    /* */
+    /* TIOA OR TIOB EXTERNAL TRIGGER SELECTION */
+    /* ABETRG = 0 (TIOA is used) <===== take default */
+    /* 1 (TIOB is used) */
+    /* */
+    /* RC COMPARE TRIGGER ENABLE */
+    /* CPCTRG = 0 (RC Compare has no effect on the counter and its clock) */
+    /* 1 (RC Compare resets the counter and starts the clock) <===== we select this one */
+    /* */
+    /* WAVE */
+    /* WAVE = 0 Capture Mode is enabled <===== we select this one */
+    /* 1 Waveform Mode is enabled */
+    /* */
+    /* RA LOADING SELECTION */
+    /* LDRA = 00 none) <===== take default */
+    /* 01 (rising edge of TIOA) */
+    /* 10 (falling edge of TIOA) */
+    /* 11 (each edge of TIOA) */
+    /* */
+    /* RB LOADING SELECTION */
+    /* LDRB = 00 (none) <===== take default */
+    /* 01 (rising edge of TIOA) */
+    /* 10 (falling edge of TIOA) */
+    /* 11 (each edge of TIOA) */
+    /* */
+    /* TCCLKS = 1 (TIMER_CLOCK5) */
+    /* CPCTRG = 1 (RC Compare resets the counter and restarts the clock) */
+    /* WAVE = 0 (Capture mode enabled) */
+    pTC->TC_CMR = 0x4004;
+
+    /* TC Register C TC_RC (read/write) Compare Register 16-bits */
+    /* */
+    /* |----------------------------------|----------------------------------------| */
+    /* | not used RC | */
+    /* |----------------------------------|----------------------------------------| */
+    /* 31 16 15 0 */
+    /* */
+    /* Timer Calculation: What count gives 1 msec time-out? */
+    /* */
+    /* TIMER_CLOCK5 = MCK / 1024 = 48054841 / 1024 = 46928 hz */
+    /* */
+    /* TIMER_CLOCK5 Period = 1 / 46928 = 21.309239686 microseconds */
+    /* */
+    /* A little algebra: .001 sec = count * 21.3092396896*10**-6 */
+    /* count = .001 / 21.3092396896*10**-6 */
+    /* count = 46.928 */
+    /* */
+    /* STK: Even Simpler, let the compiler do the work: */
+    /* */
+    /* TIMER_CLOCK5 = (MCK / 1024) / 1000 */
+    /* = 48054841 / 1024 / 1000 = 46.928 */
+
+
+    //MASKA
+    /* Timer Calculation: What count gives 100 msec time-out? */
+    /* */
+    /* TIMER_CLOCK5 = MCK / 128 = 48054841 / 1024 = 375428 hz */
+    /* */
+    /* TIMER_CLOCK5 Period = 1 / 375428 = 2.663626581 microseconds */
+    /* */
+    /* A little algebra: .000001 sec (1 msec) = count * 2.663626581*10**-6 */
+    /* count = .000001 / 2.663626581*10**-6 */
+    /* count = 1 */
+    /* */
+
+    pTC->TC_RC = 1;
+
+    /* TC Interrupt Enable Register TC_IER (write-only) */
+    /* */
+    /* */
+    /* |------------|-------|-------|-------|-------|--------|--------|--------|--------| */
+    /* | ETRGS LDRBS LDRAS CPCS CPBS CPAS LOVRS COVFS | */
+    /* |------------|-------|-------|-------|-------|--------|--------|--------|--------| */
+    /* 31 8 7 6 5 4 3 2 1 0 */
+    /* */
+    /* COVFS = 0 no effect <===== take default */
+    /* 1 enable counter overflow interrupt */
+    /* */
+    /* LOVRS = 0 no effect <===== take default */
+    /* 1 enable load overrun interrupt */
+    /* */
+    /* CPAS = 0 no effect <===== take default */
+    /* 1 enable RA compare interrupt */
+    /* */
+    /* CPBS = 0 no effect <===== take default */
+    /* 1 enable RB compare interrupt */
+    /* */
+    /* CPCS = 0 no effect */
+    /* 1 enable RC compare interrupt <===== we select this one */
+    /* */
+    /* LDRAS = 0 no effect <===== take default */
+    /* 1 enable RA load interrupt */
+    /* */
+    /* LDRBS = 0 no effect <===== take default */
+    /* 1 enable RB load interrupt */
+    /* */
+    /* ETRGS = 0 no effect <===== take default */
+    /* 1 enable External Trigger interrupt */
+    /* */
+    /* enable RC compare interrupt */
+    pTC->TC_IER = 0x10;
+
+    /* TC Interrupt Disable Register TC_IDR (write-only) */
+    /* */
+    /* */
+    /* |------------|-------|-------|-------|-------|--------|--------|--------|--------| */
+    /* | ETRGS LDRBS LDRAS CPCS CPBS CPAS LOVRS COVFS | */
+    /* |------------|-------|-------|-------|-------|--------|--------|--------|--------| */
+    /* 31 8 7 6 5 4 3 2 1 0 */
+    /* */
+    /* COVFS = 0 no effect */
+    /* 1 disable counter overflow interrupt <===== we select this one */
+    /* */
+    /* LOVRS = 0 no effect */
+    /* 1 disable load overrun interrupt <===== we select this one */
+    /* */
+    /* CPAS = 0 no effect */
+    /* 1 disable RA compare interrupt <===== we select this one */
+    /* */
+    /* CPBS = 0 no effect */
+    /* 1 disable RB compare interrupt <===== we select this one */
+    /* */
+    /* CPCS = 0 no effect <===== take default */
+    /* 1 disable RC compare interrupt */
+    /* */
+    /* LDRAS = 0 no effect */
+    /* 1 disable RA load interrupt <===== we select this one */
+    /* */
+    /* LDRBS = 0 no effect */
+    /* 1 disable RB load interrupt <===== we select this one */
+    /* */
+    /* ETRGS = 0 no effect */
+    /* 1 disable External Trigger interrupt <===== we select this one */
+    /* */
+    /* disable all except RC compare interrupt */
+    pTC->TC_IDR = 0xEF;
+}
+
 static void InitIRQ()
 {
     // IRQ0 initialization
@@ -338,17 +602,10 @@ static void InitIRQ()
     //AT91F_AIC_ConfigureIt(AT91C_BASE_AIC, AT91C_ID_FIQ, FIQ_INTERRUPT_LEVEL, AT91C_AIC_SRCTYPE_EXT_NEGATIVE_EDGE, (void(*)(void)) FIQHandler);
     //AT91F_AIC_EnableIt(AT91C_BASE_AIC, AT91C_ID_FIQ);
 
-    //Open timer0
-    AT91F_TC_Open(AT91C_BASE_TC0, TC_CLKS_MCK8, AT91C_ID_TC0);
-    //Open Timer 0 interrupt
-    AT91F_AIC_ConfigureIt(AT91C_BASE_AIC, AT91C_ID_TC0, TIMER0_INTERRUPT_LEVEL, AT91C_AIC_SRCTYPE_INT_HIGH_LEVEL, SoftIQRHandlerTC0);
-    AT91C_BASE_TC0->TC_IER = AT91C_TC_CPCS;             // IRQ enable CPC
-    AT91F_AIC_EnableIt(AT91C_BASE_AIC, AT91C_ID_TC0);
-    //Start timer0
-    AT91C_BASE_TC0->TC_CCR = AT91C_TC_SWTRG ;
+    Timer0Setup();
 
     //Open timer1
-    AT91F_TC_Open(AT91C_BASE_TC1, TC_CLKS_MCK2, AT91C_ID_TC1);
+    AT91F_TC_Open(AT91C_BASE_TC1, TC_CLKS_MCK8, AT91C_ID_TC1);
     //Open Timer 1 interrupt
     AT91F_AIC_ConfigureIt (AT91C_BASE_AIC, AT91C_ID_TC1, TIMER1_INTERRUPT_LEVEL, AT91C_AIC_SRCTYPE_INT_HIGH_LEVEL, SoftIQRHandlerTC1);
     AT91C_BASE_TC1->TC_IER = AT91C_TC_CPCS;             // IRQ enable CPC
@@ -438,13 +695,6 @@ int main(void)
 
             char *cmdParts;
             cmdParts = strtok((char*) cdcMessageObj.data, "#" );
-
-            //PERIOD#100
-            if (strcmp((char*) cmdParts, "PERIOD") == 0)
-            {
-                tempPeriod = atoi(strtok( NULL, "#" ));
-                continue;
-            }
 
             if (strcmp((char*) cmdParts, "GETID") == 0)
             {
@@ -674,16 +924,38 @@ int main(void)
                 batteryVoltageReadings = 0;
                 continue;
             }
-            //RADARROTATIONSET#1
-            //RADARROTATIONSET#2
-            //RADARROTATIONSET#3
-            //RADARROTATIONSET#4
-            //RADARROTATIONSET#5
-            //RADARROTATIONSET#6
-            //RADARROTATIONSET#7
-            //RADARROTATIONSET#8
-            //RADARROTATIONSET#9
-            //RADARROTATIONSET#10
+            //RADARROTATIONSET#700
+            //RADARROTATIONSET#750
+            //RADARROTATIONSET#800
+            //RADARROTATIONSET#850
+            //RADARROTATIONSET#900
+            //RADARROTATIONSET#950
+            //RADARROTATIONSET#1000
+            //RADARROTATIONSET#1050
+            //RADARROTATIONSET#1100
+            //RADARROTATIONSET#1150
+            //RADARROTATIONSET#1200
+            //RADARROTATIONSET#1250
+            //RADARROTATIONSET#1300
+            //RADARROTATIONSET#1350
+            //RADARROTATIONSET#1400
+            //RADARROTATIONSET#1450 //CENTER
+            //RADARROTATIONSET#1500
+            //RADARROTATIONSET#1550
+            //RADARROTATIONSET#1600
+            //RADARROTATIONSET#1650
+            //RADARROTATIONSET#1700
+            //RADARROTATIONSET#1750
+            //RADARROTATIONSET#1800
+            //RADARROTATIONSET#1850
+            //RADARROTATIONSET#1900
+            //RADARROTATIONSET#1950
+            //RADARROTATIONSET#2000
+            //RADARROTATIONSET#2050
+            //RADARROTATIONSET#2100
+            //RADARROTATIONSET#2150
+            //RADARROTATIONSET#2200
+
             if (strcmp((char*) cmdParts, "RADARROTATIONSET") == 0)
             {
                 radarServoRotation = atoi(strtok( NULL, "#" ));
