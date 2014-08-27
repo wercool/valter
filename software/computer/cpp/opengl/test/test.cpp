e2804fb0c559fa3d0ef44e57ee032241c587a21f
maska <maska@mail.ru>
2014-08-28 00:48:20 +0300

    opengl simple sample

---
+++ software/computer/cpp/opengl/test/test.cpp
@@ -0,0 +1,52 @@
+//============================================================================
+// Name        : test.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glut.h>
+
+
+/* You must have a display callback that GLUT calls to draw everything. */
+void displayCall()
+{
+    //glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
+
+    glMatrixMode( GL_PROJECTION );
+    glPushMatrix();
+    glLoadIdentity();
+    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+
+    glMatrixMode( GL_MODELVIEW );
+    glPushMatrix();
+    glLoadIdentity();
+    glRasterPos2i(10, 1000);  // move in 10 pixels from the left and bottom edges
+    glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, 'A');
+    glPopMatrix();
+
+    glMatrixMode( GL_PROJECTION );
+    glPopMatrix();
+    glMatrixMode( GL_MODELVIEW );
+
+    glutSwapBuffers();
+} /* end func displayCall */
+/**********************************************************************************************************************************/
+/* Set up everything, and start the GLUT main loop. */
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(300, 200);
+    glutCreateWindow("Hello World!");
+    glutDisplayFunc(displayCall);
+    glutFullScreen();
+    glutMainLoop();
+    return 0;
+} /* end func main */
