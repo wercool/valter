9d23a76135631f15bb08e380a77222b8da5aabff
maska <maska@devstation>
2016-11-08 02:26:12 +0200

    dummy

--- software/ros/workspaces/valter1/src/valter/src/cmd_vel_subscriber.cpp
+++ software/ros/workspaces/valter1/src/valter/src/cmd_vel_subscriber.cpp
@@ -67,7 +67,7 @@ int main(int argc, char** argv)
 
       int sock = socket(AF_INET , SOCK_STREAM , 0);
       struct sockaddr_in server;
-      server.sin_addr.s_addr = inet_addr("192.168.0.251");
+      server.sin_addr.s_addr = inet_addr("192.168.101.101");
       server.sin_family = AF_INET;
       server.sin_port = htons(55555);
 
