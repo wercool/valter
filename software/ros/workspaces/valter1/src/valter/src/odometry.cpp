fd772752d7c6d0459f44becc64033ee867957a89
maska <maska@mail.ru>
2017-06-19 14:47:27 +0300

    ros: odometry (initial commit)

---
+++ software/ros/workspaces/valter1/src/valter/src/odometry.cpp
@@ -0,0 +1,12 @@
+//wheelRadius=135mm
+//ticksPerRevolution=60
+
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
+
+int main(int argc, char** argv)
+{
+
+}
+
