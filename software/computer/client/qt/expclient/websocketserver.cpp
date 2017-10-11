f204247ff93da3bb756461afad728623fc50ec25
maska <maska@mail.ru>
2017-10-11 19:24:06 +0300

    expclient: ValterWebSocket Server in development

--- software/computer/client/qt/expclient/websocketserver.cpp
+++ software/computer/client/qt/expclient/websocketserver.cpp
@@ -64,12 +64,124 @@ void WebSocketServer::processTextMessage(QString message)
     std::string cmdValue = message_str_values[1];
     std::string cmdResponse = "OK";
 
-    switch (Valter::str2int(cmdValue.c_str()))
+    PlatformControlP1 *platformControlP1 = PlatformControlP1::getInstance();
+
+    if (Valter::str2int(cmdType.c_str()) == Valter::str2int("SRV"))
     {
-        case Valter::str2int("CLIENT READY"):
-            cmdResponse = "SERVER READY";
-        break;
+        switch (Valter::str2int(cmdValue.c_str()))
+        {
+            case Valter::str2int("CLIENT READY"):
+                cmdResponse = "SERVER READY";
+            break;
+        }
+        return;
     }
+
+    //PLATFORM-CONTROL-P1 direct commands **********************************BEGIN**************************************************
+    if (Valter::str2int(cmdType.c_str()) == Valter::str2int("PCP1"))
+    {
+        switch (Valter::str2int(cmdValue.c_str()))
+        {
+            case Valter::str2int("LRF"): //LeftRightForward
+                if (platformControlP1->preparePlatformMovement())
+                {
+                    //left and right forward
+                    if (platformControlP1->setLeftMotorDirection(true) && platformControlP1->setRightMotorDirection(true))
+                    {
+                        platformControlP1->setLeftMotorActivated(true);
+                        platformControlP1->setRightMotorActivated(true);
+                    }
+                }
+            break;
+            case Valter::str2int("RF"): //RightForward
+                if (platformControlP1->preparePlatformMovement())
+                {
+                    //right forward
+                    if (platformControlP1->setRightMotorDirection(true))
+                    {
+                        platformControlP1->setRightMotorActivated(true);
+                    }
+                }
+            break;
+            case Valter::str2int("LF"): //LeftForward
+                if (platformControlP1->preparePlatformMovement())
+                {
+                    //left forward
+                    if (platformControlP1->setLeftMotorDirection(true))
+                    {
+                        platformControlP1->setLeftMotorActivated(true);
+                    }
+                }
+            break;
+            case Valter::str2int("LRB"): //LeftRightBackward
+                if (platformControlP1->preparePlatformMovement())
+                {
+                    //left and right backward
+                    if (platformControlP1->setLeftMotorDirection(false) && platformControlP1->setRightMotorDirection(false))
+                    {
+                        platformControlP1->setLeftMotorActivated(true);
+                        platformControlP1->setRightMotorActivated(true);
+                    }
+                }
+            break;
+            case Valter::str2int("RB"): //RightBackward
+                if (platformControlP1->preparePlatformMovement())
+                {
+                    //right backward
+                    if (platformControlP1->setRightMotorDirection(false))
+                    {
+                        platformControlP1->setRightMotorActivated(true);
+                    }
+                }
+            break;
+            case Valter::str2int("LB"): //LeftBackward
+                if (platformControlP1->preparePlatformMovement())
+                {
+                    //left backward
+                    if (platformControlP1->setLeftMotorDirection(false))
+                    {
+                        platformControlP1->setLeftMotorActivated(true);
+                    }
+                }
+            break;
+            case Valter::str2int("RL"): //RotateLeft
+                if (platformControlP1->preparePlatformMovement())
+                {
+                    //left backward right forward
+                    if (platformControlP1->setLeftMotorDirection(false) && platformControlP1->setRightMotorDirection(true))
+                    {
+                        platformControlP1->setLeftMotorActivated(true);
+                        platformControlP1->setRightMotorActivated(true);
+                    }
+                }
+            break;
+            case Valter::str2int("RR"): //RotateRight
+                if (platformControlP1->preparePlatformMovement())
+                {
+                    //left forward right backward
+                    if (platformControlP1->setLeftMotorDirection(true) && platformControlP1->setRightMotorDirection(false))
+                    {
+                        platformControlP1->setLeftMotorActivated(true);
+                        platformControlP1->setRightMotorActivated(true);
+                    }
+                }
+            break;
+            case Valter::str2int("LRD"): //LeftRightDeactivate
+                platformControlP1->setLeftMotorActivated(false);
+                platformControlP1->setRightMotorActivated(false);
+            break;
+            case Valter::str2int("LD"): //LeftDeactivate
+                platformControlP1->setLeftMotorActivated(false);
+            break;
+            case Valter::str2int("RD"): //RightDeactivate
+                platformControlP1->setRightMotorActivated(false);
+            break;
+            case Valter::str2int("STOP"): //Emergency Stop
+                platformControlP1->setPlatformEmergencyStop(true);
+            break;
+        }
+    }
+    //PLATFORM-CONTROL-P1 direct commands **********************************END***************************************************
 
     if (pClient)
     {
