4014ebdd1c23a31a725f853fe98536fac78767c5
Alexey M <amaistrenko@gmail.com>
2021-12-08 11:34:34 +0200

    dev commit

--- software/raspberrypi3/ValterTalks/wiki-question-talk.py
+++ software/raspberrypi3/ValterTalks/wiki-question-talk.py
@@ -4,14 +4,22 @@
 import sys
 import wikipedia
 import re
+import subprocess
+from subprocess import Popen, PIPE
 
 wikipedia.set_lang("ru")
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-def main():
-    print(sys.argv[1])
+def main():)
+    question = sys.argv[1]
+    print("Question: " + question)
+    
+    answer = question
+    
+    pipe = Popen(["bash", "/home/maska/say.sh", '"' + answer + '"'], stdout=PIPE)
+    pipe.communicate()
 
 if __name__ == "__main__":
     main()
