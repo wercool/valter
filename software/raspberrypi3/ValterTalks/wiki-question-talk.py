70f8b9674466163a8156a6a04ee2206b367fabcd
Alexey M <amaistrenko@gmail.com>
2021-12-08 11:35:52 +0200

    Wiki Talk

--- software/raspberrypi3/ValterTalks/wiki-question-talk.py
+++ software/raspberrypi3/ValterTalks/wiki-question-talk.py
@@ -12,13 +12,20 @@ wikipedia.set_lang("ru")
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-def main():)
+def main():
     question = sys.argv[1]
     print("Question: " + question)
     
+    WikiResponse = wikipedia.summary(question, sentences=5)
+    WikiResponse = re.sub(ur'.*?\((.*?)\)', ' ', WikiResponse)
+    WikiResponse = re.sub(ur'\(.*?\) —', '', WikiResponse)
+    first_dot_index = WikiResponse.find('.')
+    WikiResponse = WikiResponse[0: first_dot_index]
+    print >> sys.stdout, "Wiki Response: \n" + WikiResponse
+    
     answer = question
     
-    pipe = Popen(["bash", "/home/maska/say.sh", '"' + answer + '"'], stdout=PIPE)
+    pipe = Popen(["bash", "/home/maska/say.sh", '"' + WikiResponse + '"'], stdout=PIPE)
     pipe.communicate()
 
 if __name__ == "__main__":
