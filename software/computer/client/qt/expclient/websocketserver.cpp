09f824e14763df5d1ae4c502a25294e8136d59e8
Alexey Maistrenko <amaistrenko@gmail.com>
2024-08-26 21:27:59 +0300

    Control body pitch from Android App

--- software/computer/client/qt/expclient/websocketserver.cpp
+++ software/computer/client/qt/expclient/websocketserver.cpp
@@ -482,7 +482,7 @@ void WebSocketServer::processTextMessage(QString message)
             case Valter::str2int("BODYPITCHDOWN"): //Pitch body down
                 if (bodyControlP1->prepareBodyPitchMovement())
                 {
-                    bodyControlP1->setBodyPitchMotorDutyMax(85);
+                    bodyControlP1->setBodyPitchMotorDutyMax(75);
                     //down
                     if (bodyControlP1->setBodyPitchMovementDirection(true))
                     {
@@ -493,7 +493,7 @@ void WebSocketServer::processTextMessage(QString message)
             case Valter::str2int("BODYPITCHUP"): //Pitch body up
                 if (bodyControlP1->prepareBodyPitchMovement())
                 {
-                    bodyControlP1->setBodyPitchMotorDutyMax(75);
+                    bodyControlP1->setBodyPitchMotorDutyMax(60);
                     //up
                     if (bodyControlP1->setBodyPitchMovementDirection(false))
                     {
